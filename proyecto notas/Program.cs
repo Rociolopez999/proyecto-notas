c539f143b3cb29e42b9adfdc3ce8718a2fafa3ca
Rociolopez999 <rm867288@gmail.com>
2024-08-19 12:56:29 -0600

    Agregar archivos de proyecto.

---
+++ proyecto notas/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Notas
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("sistema de calculo de Notas");
+        Console.WriteLine("favor ingresa tu nobre");
+        string Nombre = Console.ReadLine();
+        Console.WriteLine("favor ingrese el nombre de la asignatura");
+        string Asignatura = Console.ReadLine();
+        Console.WriteLine("ingresa la primera nota del parcial");
+      int nota 1=
+
+
+
+
